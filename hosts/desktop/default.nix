2068ad49c05a03cca241e4c75840154a057b9234
Alex <alexstenmark92@gmail.com>
2025-07-13 13:01:04 +0200

    updating more things

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,17 +1,28 @@
 {
   inputs,
-  config,
   pkgs,
   ...
 }:
 
 {
   imports = [
+    inputs.hardware.nixosModules.common-cpu-amd
+    inputs.hardware.nixosModules.common-gpu-amd
+    inputs.hardware.nixosModules.common-pc-ssd
     ./hardware-configuration.nix
     ./disko-config.nix
+    ../common/global
+    ../common/users/ergho
+    # Optional components will be added here like example
+    # ../common/optional/...
+    # old version, will replace slowly
     ./../../modules/core
   ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  networking = {
+    hostName = "desktop";
+    useDHCP = true;
+  };
 
   environment.systemPackages = with pkgs; [
     git
@@ -21,4 +32,7 @@
     wofi
     waybar
   ];
+  hardware.graphics.enable = true;
+
+  system.stateVersion = "25.04";
 }
