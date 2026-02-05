ab3d60616ee4b15ed23f03ef5844cccf97e987f9
ergho <alexstenmark92@gmail.com>
2026-02-05 09:31:44 +0100

    mutable user false

--- hosts/common/users/ergho/default.nix
+++ hosts/common/users/ergho/default.nix
@@ -8,6 +8,7 @@ let
   ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
 in
 {
+  users.mutableUsers = false;
   users.users.ergho = {
     isNormalUser = true;
     shell = pkgs.bash;
