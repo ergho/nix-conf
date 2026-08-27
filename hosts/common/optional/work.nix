22a8f7bca90a84013e6b66fd0570f0020ef43c72
ergho <alexstenmark92@gmail.com>
2026-08-27 13:23:40 +0200

    fix citrix package name and updates

--- hosts/common/optional/work.nix
+++ hosts/common/optional/work.nix
@@ -56,7 +56,7 @@ in
   #  ACTION=="add|remove", SUBSYSTEM=="usb", ATTR{idVendor}=="0424", ATTR{idProduct}=="4216", TAG+="systemd", ENV{SYSTEMD_USER_WANTS}="dock-sync.service"
   #'';
   environment.systemPackages = [
-    pkgs.citrix_workspace
+    pkgs.citrix-workspace
     pkgs.chromium
     teams
     outlook
